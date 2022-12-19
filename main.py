34a651bd27a05c02d7ee166c852ebef9b4ea0de4
Abimanyu Saridjo <abimanyusaridjo@Abimanyus-MacBook-Pro.local>
2022-12-19 14:30:05 -0500

    updated logging methods, and added real time logging in app

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import PySimpleGUI as sg
 from os import path
+import yaml
+from logs.log_setup import *
 from scripts.download_top_10s import *
 from scripts.consolidate_top_10s import *
-from logs.log_setup import *
 
 """
     Description:
@@ -12,12 +13,13 @@ from logs.log_setup import *
 class SPDRTop10Generator:
     def __init__(self):
 
-        self.log = logging.getLogger(path.basename(__file__))
+        self._log = logging.getLogger(path.basename(__file__))
+        self._print = sg.Print
 
         self.__layout = [
             [
                 sg.Text("Mark's File:"), 
-                sg.Input(key="-IN-"), 
+                sg.Input(key="-INPUT-"), 
                 sg.FileBrowse(file_types=(
                     ("XLSX", "*.xlsx*"),
                     ("XLS", "*.xls*"),
@@ -29,8 +31,21 @@ class SPDRTop10Generator:
                 sg.Button("Download New Top 10s"),
                 sg.Button("Consolidate Top 10s"),
                 sg.Button("Log")
-            ]
+            ],
+            [sg.Multiline("", size=(80, 20), autoscroll=True, key='-LOG-', reroute_stdout=True, reroute_stderr=True)]
         ]
+
+        self._window = sg.Window("SPDR Top 10 Generator", self.__layout, finalize=True)
+
+    def _log_process(self, text, print = True):
+        """
+            Description:
+                - Logger for entire application
+                - Logs each process into top10s.log file and logs in real time inside the application 
+        """
+        self._log.info(text)
+        if print == True:
+            self._window['-LOG-'].update(f"- {text}\n", append=True)
 
     
     def __validate_file(self, file):
@@ -41,43 +56,59 @@ class SPDRTop10Generator:
 
         if file and path.isfile(file):
             return True
-        self.log.warning("File doesn't exist")
-        sg.popup_error("File doesn't exist")
+        self._log_process("File doesn't exist")
         return False
+
+
+    def __open_log(self):
+        """
+            Description:
+                - Reads YAML configuration file to fetch filename of logger
+        """
+        with open(yaml_path, 'r') as f:
+            config = yaml.safe_load(f)    
+            log_file = config["handlers"]["file"]["filename"]
+            if self.__validate_file(log_file):
+                with open(log_file, 'r') as l:
+                    sg.popup_scrolled(l.read(), size=(80, 20))
 
 
     def _run_program(self):
 
-        self.log.info("Launched application")
-        window = sg.Window("SPDR Top 10 Generator", self.__layout)
+        self._log_process("Launched application")
 
         while True:
-            event, values = window.read()
+            event, values = self._window.read()
+
             match event:
                 case sg.WIN_CLOSED:
-                    self.log.info("Closed application")
+                    self._log_process("Closed application")
                     break
                 case "Exit":
-                    window.close()
+                    self._log_process("Closed application")
+                    self._window.close()
                 case "Download New Top 10s":
-                    download_top_10 = DownloadTop10s("sectors")
+                    self._log_process(f"Downloading today's top 10s from Sector SPDR's website. Open log for more details.")
+                    download_top_10 = DownloadTop10s(self._log_process, "sectors")
                     download_top_10.run_program()
                 case "Consolidate Top 10s":
-                    if self.__validate_file(values["-IN-"]):
-                        consolidate_top_10s = ConsolidateTop10s("output")
-                        consolidate_top_10s.excel_file = values["-IN-"]
+                    if self.__validate_file(values["-INPUT-"]):
+                        self._log_process("Consolidating downloaded top 10s with Mark's file.")
+                        consolidate_top_10s = ConsolidateTop10s(self._log_process, "output")
+                        consolidate_top_10s.excel_file = values["-INPUT-"]
                         consolidate_top_10s.run_program()
                 case "Log":
-                    self.log.info("Reading log file")
-                    with open(yaml_path, 'r') as f:
-                        config = yaml.safe_load(f)    
-                        log_file = config["handlers"]["file"]["filename"]
-                        if self.__validate_file(log_file):
-                            with open(log_file, 'r') as l:
-                                sg.popup_scrolled(l.read())
+                    self._log_process("Reading log file")
+                    self.__open_log()
+                
+                   
                     
-
+        self._window.close()
 
 if __name__ == "__main__":
     spdr_top_10_generator = SPDRTop10Generator()
     spdr_top_10_generator._run_program()
+
+
+class SPDRTop10Generator:
+    pass
